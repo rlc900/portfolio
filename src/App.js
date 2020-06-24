338d4b8adc49eb0af3a0ae869dae37cc6bd5e841
rlc900 <rianna.cleary@gmail.com>
2020-06-24 13:29:34 -0400

    updated portfolioi

--- src/App.js
+++ src/App.js
@@ -5,20 +5,26 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <header id="header">
+      <div class="logo">
+        <span class="icon fa-gem"></span>
+      </div>
+      <div class="content">
+        <div class="inner">
+          <h1>Rianna Cleary</h1>
+          <p>Fullstack Developer that specializes in React & Ruby on Rails.</p>
+        </div>
+      </div>
+      <nav>
+        <ul>
+          <li><a href="#intro">Intro</a></li>
+          <li><a href="#work">Work</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#contact">Contact</a></li>
+          <li><a href="#elements">Elements</a></li>
+        </ul>
+      </nav>
+    </header>
     </div>
   );
 }
